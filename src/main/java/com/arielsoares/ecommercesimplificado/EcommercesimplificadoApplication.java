ba470c84e3721807ba7f3654e5acf790038747ee
Ariel Soares <ariel.sfranco@gmail.com>
2024-08-23 09:26:31 -0300

    Project created

---
+++ src/main/java/com/arielsoares/ecommercesimplificado/EcommercesimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.arielsoares.ecommercesimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arielsoares/ecommercesimplificado/EcommercesimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arielsoares.ecommercesimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercesimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
