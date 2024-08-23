1c343773f2cfafaea7545a49b705d1f2ba2a7e46
Ariel Soares <ariel.sfranco@gmail.com>
2024-08-23 15:21:07 -0300

    Product service created

---
+++ src/main/java/com/arielsoares/ecommercesimplificado/services/ProductService.java
@@ -0,0 +1,41 @@
+package com.arielsoares.ecommercesimplificado.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arielsoares.ecommercesimplificado.entities.Product;
+import com.arielsoares.ecommercesimplificado.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public Product insert(Product Product) {
+		return repository.save(Product);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	/*
+	 * public Product update(Long id, Product Product) { Product obj =
+	 * repository.getReferenceById(id); updateData(obj, Product); return
+	 * repository.save(obj); }
+	 */
+
+}
