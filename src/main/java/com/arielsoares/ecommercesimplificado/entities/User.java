9d7ee94245b0a954bb8dc3ce062c4066cf9bbc86
Ariel Soares <ariel.sfranco@gmail.com>
2024-08-23 09:57:51 -0300

    User entity created

---
+++ src/main/java/com/arielsoares/ecommercesimplificado/entities/User.java
@@ -0,0 +1,92 @@
+package com.arielsoares.ecommercesimplificado.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String username;
+	private String password;
+	private String email;
+	
+	public User() {
+	}
+	
+	public User(Long id, String login, String username, String password, String email) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
