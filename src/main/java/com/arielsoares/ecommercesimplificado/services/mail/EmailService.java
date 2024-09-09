361d1cc867204153610ab8c5b45bfad1ad552a7d
Ariel Soares <ariel.sfranco@gmail.com>
2024-09-09 09:45:17 -0300

    Last code commit

--- src/main/java/com/arielsoares/ecommercesimplificado/services/mail/EmailService.java
+++ src/main/java/com/arielsoares/ecommercesimplificado/services/mail/EmailService.java
@@ -1,6 +1,7 @@
 package com.arielsoares.ecommercesimplificado.services.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,13 @@ public class EmailService {
 
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Value("${spring.mail.username}")
+	String from;
 
 	public void sendSimpleEmail(String toEmail, String subject, String body) {
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("ariel.sfranco@gmail.com");
+		message.setFrom(from);
 		message.setTo(toEmail);
 		message.setText(body);
 		message.setSubject(subject);
